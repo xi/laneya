4a12cede8bbaa688a2d7e94f0db20a7e5d97cfe8
Tobias Bengfort <tobias.bengfort@gmx.net>
2014-10-04 10:51:48 +0200

    implement broadcast in server protocol
    havily based on
    https://twistedmatrix.com/trac/wiki/FrequentlyAskedQuestions#HowdoImakeinputononeconnectionresultinoutputonanother

--- laneya/server.py
+++ laneya/server.py
@@ -9,18 +9,41 @@ import protocol
 
 
 class ServerProtocol(protocol.Protocol):
+    def __init__(self, factory):
+        self.factory = factory
+
+    def connectionMade(self):
+        self.factory.connections.append(self)
+
+    def connectionLost(self, reason):
+        self.factory.connections.remove(self)
+
+    def broadcastUpdate(self, command, **kwargs):
+        """Broadcast an update to all connected clients."""
+
+        for connection in self.factory.connections:
+            connection.sendUpdate(command, **kwargs)
+
     def requestReceived(self, command, **kwargs):  # TODO
         if command == 'echo':
             return kwargs
         else:
-            self.sendUpdate(command, **kwargs)
+            self.broadcastUpdate(command, **kwargs)
             return {}
+
+
+class ServerProtocolFactory(Factory):
+    def __init__(self):
+        self.connections = []
+
+    def buildProtocol(self, addr):
+        return ServerProtocol(self)
 
 
 def main():
     log.startLogging(sys.stdout)
     endpoint = TCP4ServerEndpoint(reactor, 5001)
-    endpoint.listen(Factory.forProtocol(ServerProtocol))
+    endpoint.listen(ServerProtocolFactory())
     reactor.run()
 
 
